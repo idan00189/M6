d8acfc58539fbfaa64b32b09bc4d6bf229ac6365
idan cohen <idan000189@gmail.com>
2022-05-01 16:43:17 +0300

    Initial Commit

---
+++ M6/M6App.swift
@@ -0,0 +1,17 @@
+//
+//  M6App.swift
+//  M6
+//
+//  Created by idan Cohen on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct M6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

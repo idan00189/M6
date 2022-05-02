f1f5c6c1d7d388a015b45d3f0d5f9c48b69c7095
idan cohen <idan000189@gmail.com>
2022-05-02 18:38:50 +0300

    - finish M6L4 (Lesson 4: The Yelp API)

--- M6/ModelViews/ContentModel.swift
+++ M6/ModelViews/ContentModel.swift
@@ -36,8 +36,47 @@ class ContentModel :  NSObject, CLLocationManagerDelegate, ObservableObject{
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        print(locations.first ?? "")
+        let userLocation = locations.first
         
-        locationManager.stopUpdatingLocation()
+        if userLocation != nil{
+            locationManager.stopUpdatingLocation()
+            getBusinesses(category: "Restaurants", location: userLocation!)
+        }
+    }
+    
+    
+    func getBusinesses(category : String , location: CLLocation){
+        
+        let urlString = "https://api.yelp.com/v3/businesses/search?latitude=\(location.coordinate.latitude)&longitude=\(location.coordinate.longitude)&categories=\(category)&limit=6"
+        
+        let url = URL(string: urlString)
+        
+        if let url = url {
+            
+            var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            
+            request.httpMethod = "GET"
+            request.addValue("Bearer 3rn9ola9nsHTHD_-LsngdhQKz9u0tSQ_kdI5RJ_kMHPFtnpqNPwo4h10zrUo4aJz6TjMeuVisGHsSFKDjoF649SbhkvoWdTFrSCoyZlvCyR6hM6SRjYb_Vwc9i1rYnYx", forHTTPHeaderField: "Authorization")
+            
+            let seasson = URLSession.shared
+            
+            let dataTask = seasson.dataTask(with: request) { data, response, error in
+                
+                if error == nil {
+                    print(response)
+                }
+                
+                
+                
+            }
+            dataTask.resume()
+            
+            
+            
+            
+            
+        }
+        
+        
     }
 }
